64d1c106b7f2de28cd59a0ba3ad8003f623ae5d5
Erick Pacheco Pedraza <eum602@users.noreply.github.com>
2019-01-10 15:10:24 -0500

    Update config.js

--- app/lib/config.js
+++ app/lib/config.js
@@ -10,20 +10,7 @@ const environments = {}
 environments.testing={
     'httpPort':4000,
     'httpsPort':4001,
-    'envName':'testing',
-    'hashingSecret':'thisIsASecret',
-    'maxChecks':5,
-    'twilio' : {
-        'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
-        'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
-        'fromPhone' : '+15005550006'
-    },
-    'templateGlobals':{
-        'appName':'UpTimeChecker',
-        'companyName':'NotARealCompany,Inc',
-        'yearCreated':'2018',
-        'baseUrl':'http://localhost:3000/'
-    }
+    'envName':'testing',    
 }
 
 //Determine which environment was passed as a command-line argument
